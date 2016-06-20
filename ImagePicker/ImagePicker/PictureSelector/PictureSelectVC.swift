c7647ba301ca79da02b206e8f5428708e9418031
zhangli <313890735@qq.com>
2016-06-20 15:41:51 +0800

    相册图片选择

--- ImagePicker/ImagePicker/PictureSelector/PictureSelectVC.swift
+++ ImagePicker/ImagePicker/PictureSelector/PictureSelectVC.swift
@@ -39,15 +39,65 @@ class PictureSelectVC: UIViewController {
 
 }
 
-extension PictureSelectVC : UICollectionViewDataSource, UICollectionViewDelegate {
+extension PictureSelectVC : UICollectionViewDataSource, UICollectionViewDelegate , UINavigationControllerDelegate, UIImagePickerControllerDelegate , PictureSelectDelegate{
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
             return   pictureImages.count + 1
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath) as! PictureCell
-        
+        cell.pictureDelegate = self
+        cell.image = (pictureImages.count == indexPath.item) ? nil : pictureImages[indexPath.item]
         return cell
+    }
+    
+    //MARK: PictureSelectDelegate
+    func photoDidSelect(cell : PictureCell){
+        /*
+         case PhotoLibrary     照片库(所有的照片，拍照&用 iTunes & iPhoto `同步`的照片 - 不能删除)
+         case SavedPhotosAlbum 相册 (自己拍照保存的, 可以随便删除)
+         case Camera    相机
+         */
+        //1.判断能否打开照片库
+        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+            print("不能打开相册")
+            return
+        }
+        
+        //2.创建图片选择器
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        // 设置允许用户编辑选中的图片
+        // 开发中如果需要上传头像, 那么请让用户编辑之后再上传
+        // 这样可以得到一张正方形的图片, 以便于后期处理(圆形)
+        vc.allowsEditing = true
+        presentViewController(vc, animated: true , completion: nil )
+        
+    }
+    
+    /**
+     选中相片之后调用
+     
+     :param: picker      促发事件的控制器
+     :param: image       当前选中的图片
+     :param: editingInfo 编辑之后的图片
+     */
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        pictureImages.append(image)
+        collectionV.reloadData()
+        
+        //如果实现了该方法，需要我们自己关闭图片选择器
+        picker.dismissViewControllerAnimated(true , completion: nil )
+    }
+    
+    func photoDidRemove(cell: PictureCell) {
+        //1.从数组移除当前点击的图片
+        let indexPath = collectionV.indexPathForCell(cell)
+        pictureImages.removeAtIndex((indexPath?.item)!)
+        //2.刷新表格
+        collectionV.reloadData()
+        
     }
     
 }
@@ -57,12 +107,36 @@ class LineLayout: UICollectionViewFlowLayout {
         super.prepareLayout()
         itemSize = CGSize(width: 80, height: 80)
         minimumLineSpacing = 10
-        sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        sectionInset = UIEdgeInsetsMake(20, 10, 10, 10)
     }
 }
 
 
+@objc
+protocol PictureSelectDelegate {
+    optional func photoDidSelect(cell : PictureCell)
+    optional func photoDidRemove(cell : PictureCell)
+    
+}
 class  PictureCell : UICollectionViewCell {
+    ///声明代理
+    weak  var pictureDelegate : PictureSelectDelegate?
+    
+    var image : UIImage?{
+        didSet{
+            if (image != nil ){
+                addBtn.setBackgroundImage(image!, forState: UIControlState.Normal)
+                removeBtn.hidden = false
+                self.contentView.bringSubviewToFront(removeBtn)
+                addBtn.userInteractionEnabled = false
+            }else {
+                removeBtn.hidden = true
+                addBtn.userInteractionEnabled = true 
+                addBtn.setBackgroundImage(UIImage(named:"compose_pic_add"), forState: UIControlState.Normal)
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.redColor()
@@ -93,19 +167,19 @@ class  PictureCell : UICollectionViewCell {
     
     func removeBtnClick(){
 //        YLLog("removeBtnClick")
-        print("removeBtnClick")
+        pictureDelegate?.photoDidRemove!(self)
     }
     
     func addBtnClick(){
 //        YLLog("addBtnClick")
-        print("addBtnClick")
+        pictureDelegate?.photoDidSelect!(self)
     }
     
     
     //MARK: 懒加载
     private lazy var removeBtn : UIButton = {
         let btn = UIButton()
-        btn.userInteractionEnabled = true
+//        btn.hidden = true
         btn.setImage(UIImage(named:"compose_photo_close"), forState: UIControlState.Normal)
         btn.addTarget(self , action: #selector(PictureCell.removeBtnClick), forControlEvents: UIControlEvents.TouchUpInside)
         return btn
